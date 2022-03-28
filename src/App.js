51904b9fa6407b01d204320973da6f5a06011369
Namury <namuryah@gmail.com>
2022-03-28 19:47:35 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,61 +1,28 @@
-function App() {
-  return (
-    <div className="">
-      {/* <header className="text-3xl font-bold underline text-center">
-          Real Time Chat
-      </header> */}
-      <div class="flex h-screen border">
-        <div class="flex flex-col w-full h-full p-8">
-          <div class="flex-grow">
-            <div>
-              lorem1: Lorem Ipsum Dolor Sit Amet
-            </div>
-            <div>
-              lorem2: Dolor Sit Amet Lorem Ipsum
-            </div>
-            <div>
-              lorem1: Lorem Ipsum Dolor Sit Amet
-            </div>
-            <div>
-              lorem2: Dolor Sit Amet Lorem Ipsum
-            </div>
-            <div>
-              lorem1: Lorem Ipsum Dolor Sit Amet
-            </div>
-            <div>
-              lorem2: Dolor Sit Amet Lorem Ipsum
-            </div>
-            <div>
-              lorem1: Lorem Ipsum Dolor Sit Amet
-            </div>
-            <div>
-              lorem2: Dolor Sit Amet Lorem Ipsum
-            </div>
-            <div>
-              lorem1: Lorem Ipsum Dolor Sit Amet
-            </div>
-            <div>
-              lorem2: Dolor Sit Amet Lorem Ipsum
-            </div>
-          </div>
-          <div class="flex w-full">
-            <input type="textarea" class="form-textarea flex-grow mr-4"/> 
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded flex-initial w-24">
-              Send
-            </button>
-          </div>
-        </div>
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { Suspense } from 'react';
 
-        <div class="flex flex-col w-full">
-          <div class="flex align-center justify-center aspect-video">
-            <video class="h-12 sm:h-52 lg:h-96" autoplay playsinline poster="https://thetechnoskeptic.com/wp-content/uploads/2018/05/BlackBoxComposite_iStock_leolintangivanmollov_900.jpg"></video>
-          </div>
-          <div class="flex justify-center aspect-video">
-            <video class="h-12 sm:h-52 lg:h-96"autoplay playsinline poster="https://thetechnoskeptic.com/wp-content/uploads/2018/05/BlackBoxComposite_iStock_leolintangivanmollov_900.jpg"></video>
-          </div>
-        </div>
-        
-      </div>
+const Chat = React.lazy(() => import('./pages/chat'));
+const Login = React.lazy(() => import('./pages/login'));
+const Room = React.lazy(() => import('./pages/room'));
+
+
+function App() {
+  
+  console.log('rendered, App.js');
+
+  return (
+
+    <div className="App">
+      <Suspense fallback={<div />}>
+        <Router>
+          <Routes>
+            <Route index path={"/chat"} element={<Chat> </Chat>}></Route>
+            <Route index path={"/room"} element={<Room> </Room>}></Route>
+            <Route index path={"/login"} element={<Login> </Login>}></Route>
+          </Routes>
+        </Router>
+      </Suspense>
     </div>
   );
 }
